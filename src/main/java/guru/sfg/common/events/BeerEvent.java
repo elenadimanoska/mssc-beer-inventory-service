1900688ceeeb4fee1e719e5365ccc19bfa9b679a
Elena Dimanoska <dimanoskaelena@gmail.com>
2026-04-20 23:43:32 +0200

    -Configure JMS -Implement New Inventory Listener

---
+++ src/main/java/guru/sfg/common/events/BeerEvent.java
@@ -0,0 +1,17 @@
+package guru.sfg.common.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class BeerEvent implements Serializable {
+    private static final long serialVersionUID = -2733018213794847139L;
+    private BeerDto beerDto;
+}
